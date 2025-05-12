3911a5f0a576b10ee2945771f42549b20ef3c6c0
Francesco Trolli <franz.trolli@gmail.com>
2025-05-12 14:59:17 +0200

    dct for 2D

---
+++ JPEG/dct2D.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from JPEG.dct import compute_D
+
+
+def dct_2D(f_mat):
+    N = f_mat.shape[0]
+    D = compute_D(N)
+
+    # Applica DCT per colonne
+    c_mat = D @ f_mat
+
+    # Applica DCT per righe
+    c_mat = (D @ c_mat.T).T
+
+    return c_mat
+
+def idct_2D(c_mat):
+    N = len(c_mat)
+    D = compute_D(N)
+
+    # Applica IDCT per colonne
+    f_mat = D.T @ c_mat
+
+    # Applica IDCT per righe
+    f_mat = (D.T @ f_mat.T).T
+
+    return c_mat
+
+
+if __name__ == "__main__":
+    N = 4
+    f_mat = np.random.rand(N, N)
+    c_mat = dct_2D(f_mat)
+    print(c_mat)
