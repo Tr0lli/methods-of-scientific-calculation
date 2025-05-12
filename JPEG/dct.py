a8c6ba4d3c5eae8bab396b33717b2aa35043b1b1
Francesco Trolli <franz.trolli@gmail.com>
2025-05-12 14:37:24 +0200

    add dct and idct for jpeg and compare then

--- JPEG/dct.py
+++ JPEG/dct.py
@@ -1,38 +1,86 @@
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')  # oppure 'QtAgg', se preferisci
+import matplotlib.pyplot as plt
 
 def compare_dct(N, f):
-    fj = []
-    for j in range(0, N-1):
-        fj[j] = f((2*j +1)/(2*N))
+    # 1. Valutazione della funzione f nei punti (2j+1)/(2N)
+    fj = np.array([f((2 * j + 1) / (2 * N)) for j in range(N)])
 
-    c = dct(N, fj)
-    c_troncato = c[0:N/2]
+    # 2. Calcolo DCT
+    c = dct_1D(fj)
 
-    f_new = idct(N, c_troncato)
+    # 3. Tronca i coefficienti alla metà
+    c_troncato = np.copy(c)
+    c_troncato[N // 2:] = 0
+
+    # 4. Ricostruzione tramite IDCT
+    f_new = idct_1D(c_troncato)
+
+    # 5. Visualizzazione
+    plt.figure(figsize=(10, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(fj, label="Originale")
+    plt.plot(f_new, label="Ricostruito (IDCT)")
+    plt.legend()
+    plt.title("Confronto f(x) e IDCT")
+
+    plt.subplot(1, 2, 2)
+    plt.stem(c, linefmt='C0-', markerfmt='C0o', basefmt='k-', label='DCT completa')
+    plt.stem(c_troncato, linefmt='C1--', markerfmt='C1s', basefmt='k-', label='DCT troncata')
+    plt.legend()
+    plt.title("Coefficienti DCT")
+    plt.tight_layout()
+    plt.show()
 
     return c_troncato, f_new
 
 
+def compute_D(N):
+    D = np.zeros((N, N))
+    alpha = np.ones(N) * np.sqrt(2 / N)
+    alpha[0] = 1 / np.sqrt(N)
 
-def dct(N, fj):
-    c = []
-    for k in range(0, N-1):
-        if k == 0:
-            alpha = 1/np.sqrt(N)
-        else:
-            alpha = np.sqrt(2/N)
-        for j in range(0, N-1):
-            c[k] = alpha * (fj[j] * np.cos(k*np.pi*((2*j+1)/(2*N))))
-    return c
+    for k in range(N):
+        for i in range(N):
+            D[k, i] = alpha[k] * np.cos((k) * np.pi * (2 * i + 1) / (2 * N))
+    return D
 
-def idct(N, c):
-    fj = []
-    for j in range(0, N-1):
-        for k in range(0, N-1):
-            if k == 0:
-                alpha = 1/np.sqrt(N)
-            else:
-                alpha = np.sqrt(2/N)
+def dct_1D(f_vect):
+    N = len(f_vect)
+    D = compute_D(N)
+    c_vect = D @ f_vect  # prodotto matrice-vettore
+    """
+    plt.figure()
+    plt.bar(np.arange(N), c_vect)
+    plt.title("DCT Coefficients")
+    plt.show()
+    """
+    return c_vect
 
-            fj[j] = c[k] * alpha * np.cos(k*np.pi*((2*j+1)/(2*N)))
-    return fj
+def idct_1D(c_vect):
+    N = len(c_vect)
+    D = compute_D(N)
+    f_vect = D.T @ c_vect  # inversa della DCT: trasposizione
+    """
+    plt.figure()
+    plt.bar(np.arange(N), f_vect)
+    plt.title("Ricostruzione tramite IDCT")
+    plt.show()
+    """
+    return f_vect
+
+# Esempio di utilizzo
+
+if __name__ == "__main__":
+    N = 32
+    f = lambda x: np.sin(2 * np.pi * x) + 0.5 * np.cos(4 * np.pi * x)
+    compare_dct(N, f)
+
+"""
+if __name__ == "__main__":
+    N = 16
+    f_vect = np.array([np.sin(np.pi * (2 * i + 1) / (2 * N)) for i in range(N)])
+    c = dct_1D(f_vect)
+    f_ricostruito = idct_1D(c)
+"""
